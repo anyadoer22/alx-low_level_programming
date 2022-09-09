7430d5b43c765b8cc005dd33202505e0e1742c48
root <anyadoer22>
2022-09-09 09:44:34 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 /**
  * main - starting point
- * 
+ *
  *  Return: 0
  *
  */
 int main(void)
 {
 	char alpha;
+
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 	putchar(alpha);
 	}
-	putchar('\n');	
+	putchar('\n');
 	 return (0);
 }
