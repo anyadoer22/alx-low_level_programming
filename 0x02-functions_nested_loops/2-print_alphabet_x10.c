84b5d771fc4017495d8f8e455b44c1f37199f10d
root <anyadoer22>
2022-09-14 09:00:27 -0700

    My firt commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10x the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char letter;
+	for (n = 1; n <= 10; n++)
+	{
+	for (strt = 97; strt < 123; strt++)
+	{
+	_putchar(letter);
+	}
+	_putchar('\n');
+	}
+}
