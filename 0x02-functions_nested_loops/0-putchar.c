6b43bce91ddacf9a9488c545d49b57ce234f9ea6
root <anyadoer22>
2022-09-14 08:36:57 -0700

    My first putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *
+ * main - Prints _putchar followed by a newline
+ *
+ *
+ *
+ * Return: Always 0.
+ *
+ */
+int main(void)
+{
+	int putChar[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int cnt, len;
+	len = sizeof(putChar) / sizeof(int);
+	for (cnt = 0; cnt < len; cnt++)
+	{		
+	_putchar(putChar[cnt]);
+	}	
+	_putchar('\n');
+	return (0);
+}
