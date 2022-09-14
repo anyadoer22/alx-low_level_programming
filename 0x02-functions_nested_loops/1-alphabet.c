79d229f363ceb99a2193fc78ee35b538f9940d32
root <anyadoer22>
2022-09-14 08:53:59 -0700

    My firt commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return : Always 0
+ */
+void print_alphabet(void)
+{
+	int strt;
+	for (strt = 97; strt < 123; strt++)
+	{
+	_putchar(strt);
+	}
+	_putchar('\n');
+}
