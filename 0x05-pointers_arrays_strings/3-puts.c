0e589fdbea5e1dc7df4a6b5567e408cc7797082d
root <anyadoer22>
2022-09-19 17:06:10 -0700

    Pointers and Array

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - primts a string to standard output
+ * @str: the string to be printed
+ */
+void _puts(char *str)
+{
+	while (*str)
+	_putchar(*str++);
+	_putchar('\n');
+}
