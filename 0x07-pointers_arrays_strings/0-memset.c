a97fdb116e3252b18586e258ca02366834735685
root <anyadoer22>
2022-09-27 18:37:46 -0700

    HMM

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,46 +1,20 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of memory to print
+ * _memset - Fill memory with a constant byte
+ * @s: memory area to fill
+ * @b: constant byte to fill
+ * @n: bytes of memory area to fill
  *
- * Return: Nothing
+ * Return: the memory area filled
  */
- void simple_print_buffer(char *buffer, unsigned int size)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int a;
 
-	i = 0;
-	while (i < size)
+	for (a = 0; a < n; a++)
 	{
-	if (i % 10)
-	{
-	printf(" ");
+	s[a] = b;
 	}
-	if (!(i % 10) && i)
-	{
-	printf("\n");
-	}
-	printf("0x%02x", buffer[i]);
-	i++;
-	}
-	printf("\n");
-}
-
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char buffer[98] = {0x00};
-
-	simple_print_buffer(buffer, 98)
-	_memset(buffer, 0x01, 95);
-	printf("-------------------------------------------------\n");
-	simple_print_buffer(buffer, 98);
-	return (0);
+	return (s);
 }
